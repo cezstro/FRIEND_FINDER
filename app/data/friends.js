6d4be84c85d2f7c890d6a8ea900b7f8eace7f270
Cezstro <raheem.hardy@gmail.com>
2018-01-16 21:55:46 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Dave",
+    photo: "https://memegenerator.net/img/images/600x600/9995327/chappelle-crackhead.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Crazy Eyes",
+    photo: "https://memegenerator.net/img/images/250x250/9889410/crazy-eyes-orange-is-the-new-black.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Willy Wonka",
+    photo: "https://memegenerator.net/img/images/600x600/2729805/willy-wonka.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Bossman",
+    photo: "https://memegenerator.net/img/images/600x600/7665331/that-would-be-great.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Say Hello",
+    photo: "https://memegenerator.net/img/images/600x600/8370805/tony-montana-scarface.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Tony",
+    photo: "https://memegenerator.net/img/images/600x600/11534184/tony-stark-open-arms.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
